0f5a0d458c1cc00e3ce3d5e37686ef19060607ba
leviwaynedaily <leviwaynedaily@gmail.com>
2025-07-31 22:32:49 -0400

    Export ZoneDevicesModal from ui components index

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,7 @@
 export { LiveEventsTicker } from './LiveEventsTicker';
 export { PinEntry } from './PinEntry';
 export { ZoneStatus } from './ZoneStatus';
+export { ZoneDevicesModal } from './ZoneDevicesModal';
 export { Clock } from './Clock';
 export { IconPicker } from './IconPicker';
 export { ProcessingOverlay } from './ProcessingOverlay';
